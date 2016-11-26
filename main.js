539fcfef312e49bed295e39580656c200f5aef01
Esther <esther@showgizmo.com>
2016-11-26 20:35:36 +1300

    add jquery to tabs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	var currentTab = "me"
+
 	var counter = document.getElementById('counter');
 	if (counter) {
 		document.getElementById('counter').innerHTML = ensureCommas(counter.innerHTML);
@@ -9,12 +11,14 @@ $(document).ready(function() {
 		if (!$(this).hasClass("active")) {
 			$(".lt-green-btn").removeClass("active");
 			$(this).addClass("active");
+			currentTab = $(this).attr("id");
+			if (window.location.pathname.includes("donate")) {
+				switchContextDonations(currentTab)
+			} else if (window.location.pathname.includes("results")) {
+				switchContextActions(currentTab)
+			}
 		}
 	});
-
-
-$(".thumbs:not(.selected)").hover()
-
 });
 
 function increment() {
@@ -39,5 +43,49 @@ function numberWithCommas(x) {
 
 function ensureCommas(x) {
 	return x.toString().replace(",", "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function switchContextDonations(currentTab) {
+	switch (currentTab) {
+		case 'me':
+			$("#capsule")[0].textContent = "$5";
+			break;
+		case 'everyone':
+			$("#capsule")[0].textContent = "$12,349"
+			break;
+		case 'all-time':
+			$("#capsule")[0].textContent = "$508,370"
+			break;
+		default:
+			$("#capsule")[0].textContent = "$12,349"
+	}
+}
+
+function switchContextActions(currentTab) {
+	switch (currentTab) {
+		case 'me':
+			$("#capsule")[0].textContent = "5";
+			$("#electricity")[0].textContent = "3,000 watts of electricity";
+			$("#co2")[0].textContent = "0.04 tonnes of CO2";
+			$("#water")[0].textContent = "0.2 litres of water";
+			break;
+		case 'everyone':
+			$("#capsule")[0].textContent = "12,349"
+			$("#electricity")[0].textContent = "743,000 watts of electricity";
+			$("#co2")[0].textContent = "53 tonnes of CO2";
+			$("#water")[0].textContent = "430 litres of water";
+			break;
+		case 'all-time':
+			$("#capsule")[0].textContent = "508,370"
+			$("#electricity")[0].textContent = "17,847,800 watts of electricity";
+			$("#co2")[0].textContent = "391 tonnes of CO2";
+			$("#water")[0].textContent = "26,730 litres of water";
+			break;
+		default:
+			$("#capsule")[0].textContent = "12,349"
+			$("#electricity")[0].textContent = "743,000 watts of electricity";
+			$("#co2")[0].textContent = "53 tonnes of CO2";
+			$("#water")[0].textContent = "430 litres of water";
+	}
 }
 
